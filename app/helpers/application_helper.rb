7a4b1b39a124e0fdca65bc98f05b91a8cd734bf8
kikedu1 <enriduranh@gmail.com>
2023-10-19 16:51:39 -0300

    Implementando I18n

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+    def locale
+        I18n.locale == :en ? "Estados Unidos": "Português do Brasil" #ternaria invés de if else end quando são apenas duas respostas
+    end        
+    
     def ambiente_rails
         if Rails.env.development?
             "Desenvolvimento"
