dca53d930e84a30e5903e6f51150aa9583cbb37e
kikedu1 <enriduranh@gmail.com>
2023-10-19 18:01:08 -0300

    Criando cookies e sessions

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-    @meu_nome = "Kike"
-    #@meu_nome = params[:nome]
-    @curso = "Rails"
-    #@curso = params[:curso]
+    cookies[:curso] = "Curso de Ruby on Rails [COOKIE]"
+    session[:curso] = "Curso de Ruby on Rails [SESSION]"
+    @meu_nome = params[:nome]
+    @curso = params[:curso]
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
     def locale
         I18n.locale == :en ? "Estados Unidos": "Português do Brasil" #ternaria invés de if else end quando são apenas duas respostas
+        #/não usado mais, agora usando o en/pt.yml
     end        
     
     def ambiente_rails
